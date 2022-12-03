8074f6b4d533b41705c08f2ef034bc3c92d17c05
Andrey Obibok <andreyobibok@gmail.com>
2022-12-03 19:50:16 +0200

    fixed task 6

--- js/task-06.js
+++ js/task-06.js
@@ -2,10 +2,12 @@ const myInput = document.querySelector('#validation-input');
 console.log(myInput.dataset.length);
 
 myInput.addEventListener('blur', () => {
-    if (myInput.value.length != myInput.dataset.length) {
-        myInput.classList.add('invalid');
+    if (myInput.value.length == myInput.dataset.length) {
+        myInput.className = '';
+        myInput.classList.add('valid');
     }
     else {
-        myInput.classList.replace('invalid', 'valid');
+        myInput.className = '';
+        myInput.classList.add('invalid');
     }
 })
