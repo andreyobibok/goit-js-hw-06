51c054744f60e6b071bca087093775f0073ab085
Andrey Obibok <andreyobibok@gmail.com>
2022-12-04 21:17:40 +0200

    fix task 02

--- js/task-02.js
+++ js/task-02.js
@@ -7,13 +7,14 @@ const ingredients = [
   'Condiments',
 ];
 
-const list = document.querySelector('#ingredients');
-const item = ingredients.map(value => `<li class="item">${value}</li>`).join("");
-list.insertAdjacentHTML('beforeend', item);
+const list = document.querySelector('#ingredients')
+const liArray = []
 
-// ingredients.forEach(element => {
-//   const item = document.createElement('li');
-//   item.textContent = element;
-//   item.classList.add('item');
-//   list.append(item);
-// });
+ingredients.forEach(ingredient => {
+    const item = document.createElement('li')
+    item.className = 'item'
+    item.textContent = ingredient
+    liArray.push(item)
+})
+
+list.append(...liArray)
